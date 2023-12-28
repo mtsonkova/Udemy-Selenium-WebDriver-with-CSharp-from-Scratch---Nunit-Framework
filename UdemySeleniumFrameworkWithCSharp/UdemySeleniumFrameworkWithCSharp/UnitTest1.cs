172458589c384aed27777ed6b599eb5cb080d826
mtsonkova <mtsonkova950@gmail.com>
2023-12-28 23:49:10 +0200

    work in progress

--- UdemySeleniumFrameworkWithCSharp/UdemySeleniumFrameworkWithCSharp/UnitTest1.cs
+++ UdemySeleniumFrameworkWithCSharp/UdemySeleniumFrameworkWithCSharp/UnitTest1.cs
@@ -1,33 +1,59 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace UdemySeleniumFrameworkWithCSharp
 {
    
     public class Tests
     {
-        WebDriver driver;
+        IWebDriver driver;
 
         [SetUp]
         public void Setup()
         {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
             driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
         }
 
         [Test]
         public void E2ETest()
         {
-            String url = "https://rahulshettyacademy.com/angularpractice/";
+           
             String[] expectedProducts = { "iphone X", "Blackberry" };
-            driver.Navigate().GoToUrl(url);
+           
             driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
             driver.FindElement(By.Name("password")).SendKeys("learning");
-            driver.FindElement(By.XPath("//div[@class='form-group][5]/label/span/input"));
+           
             driver.FindElement(By.XPath("//input[@value='Sign In']")).Click();
 
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible);
+           
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.PartialLinkText("Checkout")));
+
+            IList<IWebElement> products =  driver.FindElements(By.TagName("app-card"));
+
+            foreach(IWebElement product in products) 
+            {
+
+                String productTitle = product.FindElement(By.CssSelector(".card-title a")).Text;
+               
+                if(expectedProducts.Contains(productTitle))
+                {
+                    IWebElement btn_Add = product.FindElement(By.CssSelector(".card-footer button"));
+
+                    btn_Add.Click();
+                }
+                
+            }
+
+
+            driver.FindElement(By.PartialLinkText("Checkout")).Click();
 
 
         }
